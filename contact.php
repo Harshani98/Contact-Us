372287492d5f1bba454004f6a6606f44f08c2148
Harshani98 <67255902+Harshani98@users.noreply.github.com>
2020-08-24 21:19:29 +0530

    Add files via upload
    contact us

---
+++ contact.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Hiroto Template">
+    <meta name="keywords" content="Hiroto, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Sea Side South Park</title>
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <!-- Offcanvas Menu Begin -->
+    <div class="offcanvas-menu-overlay"></div>
+   
+        <div class="header__nav__option">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-2">
+                        <div class="header__logo">
+                            <a href="./index.php"><img src="img/logo.png" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-10">
+                        <div class="header__nav">
+                            <nav class="header__menu">
+                                 <ul class="menu__class">
+                                    <li class="active"><a href="./index.php">Home</a></li>
+                                    <li><a href="./seasidehotel/index.html">About Us</a></li>
+                                    <li><a href="about.html">Other Hotels</a></li>
+                                    <li><a href="#">Pages</a>
+                                        <ul class="dropdown">
+                                            <li><a href="./seasidehotel/index.php">About Us</a></li>
+                                            <li><a href="./about.html">Other Hotels</a></li>
+                                            <li><a href="./register.html">Register</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="./login.php">Login</a></li>
+                                    <li><a href="./contact.html">Contact</a></li>
+                                </ul>
+                            </nav>
+                            <div class="header__nav__widget">
+                                <a href="#">Book Now <span class="arrow_right"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="canvas__open">
+                    <span class="fa fa-bars"></span>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header Section End -->
+
+    <!-- Map Begin -->
+    <div class="map">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.9283881740894!2d79.8763581147044!3d6.778570395100298!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae24503bf86363d%3A0x91372aa801c0cbef!2sSea%20Side%20Terrace%20Tourist%20Hotel!5e0!3m2!1sen!2slk!4v1598241266743!5m2!1sen!2slk"
+            height="600" style="border:0;" allowfullscreen=""></iframe>
+    </div>
+    <!-- Map End -->
+
+    <!-- Contact Section Begin -->
+    <section class="contact spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-md-6 col-sm-5">
+                    <div class="contact__widget">
+                        <div class="contact__widget__item">
+                            <h4>Contact Us</h4>
+                            <ul>
+                                <li>0112555555</li>
+                                <li>Info.seasidesouth@gmail.com</li>
+                            </ul>
+                        </div>
+                        <div class="contact__widget__item">
+                            <h4>Address</h4>
+                            <p>96 Sea Side South Hotel, Colombo<br />United Stated</p>
+                        </div>
+                        <div class="contact__widget__time">
+                            <h4>Opentime</h4>
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 col-sm-6">
+                                    <div class="contact__widget__time__item">
+                                        <ul>
+                                            <li>Monday - Friday</li>
+                                            <li><span>8 am - 9 pm</span></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-6">
+                                    <div class="contact__widget__time__item">
+                                        <ul>
+                                            <li>Saturday - Sunday</li>
+                                            <li><span>8 am - 9 pm</span></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 offset-lg-1 col-md-6 col-sm-7">
+                    <div class="contact__form">
+                        <h2>Your question?</h2>
+                        <form action="#">
+                            <input type="text" placeholder="Your Name">
+                            <input type="text" placeholder="Email">
+                            <textarea placeholder="Your Message"></textarea>
+                            <button type="submit">Send Message</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Contact Section End -->
+
+    <!-- Footer Section Begin -->
+       <footer class="footer set-bg" data-setbg="img/footer-bg.jpg">
+        
+        <div class="container">
+            <div class="footer__content">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="footer__about">
+                            <div class="footer__logo">
+                                <a href="#"><img src="img/logo.png" alt=""></a>
+                            </div>
+                            <h4>0112555555</h4>
+                            <ul>
+                                <li>96, Sea Side South Park, Colombo</li>
+                                <li>Info.seasidesouth@gmail.com</li>
+                            </ul>
+                            <div class="footer__social">
+                                <a href="https://www.facebook.com/seasidehotelandspa/"><i class="fa fa-facebook"></i></a>
+                                <a href="https://twitter.com/seasidehs"><i class="fa fa-twitter"></i></a>
+                                <a href="https://in.linkedin.com/company/seasideofficiel"><i class="fa fa-linkedin"></i></a>
+                                <a href="https://www.youtube.com/watch?v=SCM4o_lnZf4"><i class="fa fa-youtube-play"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 offset-lg-1 col-md-5 offset-md-1 col-sm-6">
+                        <div class="footer__widget">
+                            <h4>Multiple Hotels</h4>
+                            <ul>
+                                <li><a href="#">Sea Side South Park</a></li>
+                                <li><a href="#">Sea Side South Beach</a></li>
+                                <li><a href="#">Sea Beach Hotel</a></li>
+                               
+                            </ul>
+                          
+                        </div>
+                    </div>
+                    <div class="col-lg-5 col-md-8 col-sm-12">
+                        <div class="footer__newslatter">
+                            <h4>Subscribe our newlatester</h4>
+                            <form action="#">
+                                <input type="text" placeholder="Your E-mail Address">
+                                <button type="submit">Subscribe</button>
+                            </form>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+          
+        </div>
+    </footer>
+    <!-- Footer Section End -->
+
+    <!-- Js Plugins -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
